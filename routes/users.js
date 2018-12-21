ced4af6a0970db3b89ecfa493de2985d5a5ea292
naaltunian <naaltunian@icloud.com>
2018-12-21 16:34:17 -0500

    turn errors to partial and make form post for registration

--- routes/users.js
+++ routes/users.js
@@ -12,4 +12,28 @@ router.get('/login', (req, res) => {
     res.render('users/login');
 });
 
+// form post
+router.post('/register', (req, res) => {
+    let errors = [];
+    if(req.body.password != req.body.passwordConfirmation) {
+        errors.push({text: "Match Passwords"})
+    }
+
+    if(req.body.password.length < 8){
+        errors.push({text: "Password must be at least 8 characters long"})
+    }
+
+    if(errors.length > 0){
+        res.render('users/register', {
+            errors: errors,
+            name: req.body.name,
+            email: req.body.email,
+            password: req.body.password,
+            passwordConfirmation: req.body.passwordConfirmation
+        });
+    } else {
+        res.send('pass');
+    }
+})
+
 module.exports = router;
