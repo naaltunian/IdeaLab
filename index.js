18cbea8f4f7019e134892115c335ee2e5a43afe1
naaltunian <naaltunian@icloud.com>
2018-12-22 12:30:56 -0500

    conditionally render login/register based on if user is logged in

--- index.js
+++ index.js
@@ -40,6 +40,7 @@ app.use((req, res, next) => {
     res.locals.success = req.flash("success");
     res.locals.error_msg = req.flash("error_msg");
     res.locals.error = req.flash("error");
+    res.locals.user = req.user || null;
     next();
 });
 
